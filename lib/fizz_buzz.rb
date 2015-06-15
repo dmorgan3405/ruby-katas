c93272ecd06068c5e968a0b2a1c8e9ad5eb9198a
DXM742 <doug.morgan@leandogsoftware.com>
2015-06-15 11:24:45 -0400

    Fizz Buzz with Grant 6/15/2015

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,15 +1,34 @@
 class FizzBuzz
-	def self.output(input)
-		return 'Fizz' if divisible_by_three? input
-		return 'Buzz' if divisible_by_five? input
+
+	def output(input)
+		return :fizzbuzz if is_fizz_buzz?(input)
+		return :fizz if is_fizz?(input)
+		return :buzz if is_buzz?(input)
 		input
+	end	
+
+	private
+
+	def is_fizz_buzz?(input)
+		is_fizz?(input) and is_buzz?(input)
 	end
 
-	def self.divisible_by_three?(input)
-		input % 3 == 0
+	def is_fizz?(input)
+		input.divisible_by?(3) or input.contains?(3)
 	end
 
-	def self.divisible_by_five?(input)
-		input % 5 == 0
+	def is_buzz?(input)
+		input.divisible_by?(5) or input.contains?(5)
+	end
+
+end
+
+class Fixnum
+	def divisible_by?(divisor)
+		self % divisor == 0
+	end
+
+	def contains?(input)
+		self.to_s.include?(input.to_s)
 	end
 end
--- spec/katas/fizzbuzz_spec.rb
+++ spec/katas/fizzbuzz_spec.rb
@@ -2,39 +2,31 @@ require 'spec_helper'
 
 describe FizzBuzz, 'Output' do
 
-	it 'Should Return One When Given One' do 
-		actual = FizzBuzz.output 1
-		expect(actual).to eq 1
+	let(:game) { FizzBuzz.new }
+
+	[1,2,4,7,8].each do |test_case|
+		it "Should Return #{test_case} when given #{test_case}" do 
+			expect(game.output(test_case)).to eq(test_case)
+		end
 	end
 
-	it 'Should Return Two Given Two' do
-		actual = FizzBuzz.output 2
-		expect(actual).to eq 2
+	[3,6,9,13,23].each do |test_case|
+		it "Should Return Fizz when given #{test_case}" do
+			expect(game.output(test_case)).to be(:fizz)
+		end
 	end
 
-	it 'Should Return Fizz Given Three' do
-		actual = FizzBuzz.output 3
-		expect(actual).to eq 'Fizz'
+	[5,10,52,59].each do |test_case|
+		it "Should Return Buzz when given #{test_case}" do
+			expect(game.output(test_case)).to be(:buzz)
+		end
 	end
 
-	it 'Should Return Four Given Four' do
-		actual = FizzBuzz.output 4
-		expect(actual).to eq 4
+	[15,30,51,53,54,57].each do |test_case|
+		it "Should Return FizzBuzz when given #{test_case}" do
+			expect(game.output(test_case)).to be(:fizzbuzz)
+		end		
 	end
 
-	it 'Should Return Buzz Given Five' do
-		actual = FizzBuzz.output 5
-		expect(actual).to eq 'Buzz'
-	end
-
-	it 'Should Return Fizz Given Six' do
-		actual = FizzBuzz.output 6
-		expect(actual).to eq 'Fizz'
-	end
-
-	it 'Should Return Buzz Given Ten' do
-		actual = FizzBuzz.output 10
-		expect(actual).to eq 'Buzz'
-	end
 end
 
